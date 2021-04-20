84573f556ca40f73d6ba0430d8be8be8c5f0310d
fishfishz <yyu18@uw.edu>
2021-04-20 14:25:30 -0700

    firebase added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdicI_-M0lIrJRNdczUdlhtrludFMa4IA",
+    authDomain: "livingbuddy-75295.firebaseapp.com",
+    projectId: "livingbuddy-75295",
+    storageBucket: "livingbuddy-75295.appspot.com",
+    messagingSenderId: "797012290700",
+    appId: "1:797012290700:web:4612726f9f7f174b641fa9",
+    measurementId: "G-R2GEVZMFDG"
+};
+
+// Use the config value from google firebase console here
+firebase.initializeApp({
+    apiKey: "AIzaSyBdicI_-M0lIrJRNdczUdlhtrludFMa4IA",
+    authDomain: "livingbuddy-75295.firebaseapp.com",
+    projectId: "livingbuddy-75295",
+    storageBucket: "livingbuddy-75295.appspot.com",
+    messagingSenderId: "797012290700",
+    appId: "1:797012290700:web:4612726f9f7f174b641fa9",
+    measurementId: "G-R2GEVZMFDG"
+});
 
 ReactDOM.render(
   <React.StrictMode>
