830f4680d193d4b33249a36c0389e6958fa64e59
fishfishz <yyu18@uw.edu>
2021-05-04 13:58:01 -0700

    folders added

--- my-app/src/tests/App.test.js
+++ my-app/src/tests/App.test.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import React from 'react';
 import {getByTestId, render} from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 import firebase from "firebase";
 
 test('renders learn react link', () => {
