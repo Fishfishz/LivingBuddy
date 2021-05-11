9b7414203b54645eb15f7e662af973a6a4f2aca8
Chris <chrisy5@uw.edu>
2021-05-11 14:09:50 -0700

    Revert "Merge branch 'main' into chris"
    This reverts commit 4a7c137b4876b6ae80a05a4f36ca111fd1c7451b, reversing
    changes made to c759a5507ad89ce4c99c973ac689149fcf028974.

--- LivingBuddy/src/components/Navbars/AdminNavbarLinks.js
+++ LivingBuddy/src/components/Navbars/AdminNavbarLinks.js
@@ -20,28 +20,24 @@ import CustomInput from "components/CustomInput/CustomInput.js";
 import Button from "components/CustomButtons/Button.js";
 
 import styles from "assets/jss/material-dashboard-react/components/headerLinksStyle.js";
-import { Auth } from "../../configs/firebaseConfig";
-import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles(styles);
 
 export default function AdminNavbarLinks() {
   const classes = useStyles();
-  const history = useHistory();
   const [openNotification, setOpenNotification] = React.useState(null);
   const [openProfile, setOpenProfile] = React.useState(null);
-  const handleClickNotification = (event) => {
+  const handleClickNotification = event => {
     if (openNotification && openNotification.contains(event.target)) {
       setOpenNotification(null);
     } else {
       setOpenNotification(event.currentTarget);
     }
   };
-
   const handleCloseNotification = () => {
     setOpenNotification(null);
   };
-  const handleClickProfile = (event) => {
+  const handleClickProfile = event => {
     if (openProfile && openProfile.contains(event.target)) {
       setOpenProfile(null);
     } else {
@@ -51,25 +47,18 @@ export default function AdminNavbarLinks() {
   const handleCloseProfile = () => {
     setOpenProfile(null);
   };
-
-  const handleLogOut = () => {
-    Auth.signOut();
-    sessionStorage.removeItem("signedIn");
-    history.push("/sign-in");
-  };
-
   return (
     <div>
       <div className={classes.searchWrapper}>
         <CustomInput
           formControlProps={{
-            className: classes.margin + " " + classes.search,
+            className: classes.margin + " " + classes.search
           }}
           inputProps={{
             placeholder: "Search",
             inputProps: {
-              "aria-label": "Search",
-            },
+              "aria-label": "Search"
+            }
           }}
         />
         <Button color="white" aria-label="edit" justIcon round>
@@ -123,7 +112,7 @@ export default function AdminNavbarLinks() {
               id="notification-menu-list-grow"
               style={{
                 transformOrigin:
-                  placement === "bottom" ? "center top" : "center bottom",
+                  placement === "bottom" ? "center top" : "center bottom"
               }}
             >
               <Paper>
@@ -198,7 +187,7 @@ export default function AdminNavbarLinks() {
               id="profile-menu-list-grow"
               style={{
                 transformOrigin:
-                  placement === "bottom" ? "center top" : "center bottom",
+                  placement === "bottom" ? "center top" : "center bottom"
               }}
             >
               <Paper>
@@ -218,7 +207,7 @@ export default function AdminNavbarLinks() {
                     </MenuItem>
                     <Divider light />
                     <MenuItem
-                      onClick={handleLogOut}
+                      onClick={handleCloseProfile}
                       className={classes.dropdownItem}
                     >
                       Logout
--- LivingBuddy/src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { getByTestId, render } from "@testing-library/react";
-import Dashboard from "../views/Dashboard/Dashboard";
-import "@testing-library/jest-dom";
-
-test("renders learn react link", () => {
-  render(<Dashboard />);
-  expect(getByTestId(document.documentElement, "t1")).toBeInTheDocument();
-});
